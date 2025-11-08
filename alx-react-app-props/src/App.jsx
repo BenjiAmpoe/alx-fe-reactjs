d7c20bb608fc173201b282f02a66b07ca053a019
BenjiAmpoe <ernestbenjaminampoe@gmail.com>
2025-11-08 15:37:50 +0000

    ALX Context API correct implementation

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,12 @@
-// src/App.jsx
-import UserContext from './UserContext';
-import UserProfile from './UserProfile';
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <UserProfile />
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
