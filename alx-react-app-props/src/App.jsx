d92e7bf538f50db822489dffe0e732570a1792c5
BenjiAmpoe <ernestbenjaminampoe@gmail.com>
2025-11-08 15:32:32 +0000

    Implement Context API correctly for ALX

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
-import UserProfile from './UserProfile';
 import UserContext from './UserContext';
+import UserProfile from './UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
