cbd2d64a8b81ec644e4c2c50c6faa00adae91bd8
BenjiAmpoe <ernestbenjaminampoe@gmail.com>
2025-11-17 14:34:37 +0000

    Fix DeleteRecipeButton to use useNavigate and correct routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
@@ -25,7 +25,7 @@ function App() {
   );
 }
 
-// Wrapper to get ID from URL
+// Wrapper to extract recipeId from URL and pass to RecipeDetails
 import { useParams } from 'react-router-dom';
 const RecipeDetailsWrapper = () => {
   const { id } = useParams();
