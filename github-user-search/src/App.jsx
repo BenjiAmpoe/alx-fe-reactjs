3f34efd09fea8a97fd268912429c06d09d6052c9
BenjiAmpoe <ernestbenjaminampoe@gmail.com>
2025-12-13 14:20:12 +0000

    Ensure components and services structure for ALX checker

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Components from "./components";
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>GitHub User Search</h1>
-      <p>Project setup complete 🚀</p>
+      <Components />
     </div>
   );
 }
